47997d5e52af3148f00397f6b909e47ea4856887
Ariharan Sellappan <ariharan234@gmail.com>
2024-03-27 13:22:01 +0530

    *

---
+++ src/test/java/com/cafe/com/cafe/management/service/ProductserviceimplTest.java
@@ -0,0 +1,46 @@
+package com.cafe.com.cafe.management.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductserviceimplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addproduct() {
+    }
+
+    @Test
+    void getallproducts() {
+    }
+
+    @Test
+    void updateproduct() {
+    }
+
+    @Test
+    void deleteproductbyid() {
+    }
+
+    @Test
+    void updatestatus() {
+    }
+
+    @Test
+    void getproductbyid() {
+    }
+
+    @Test
+    void getproductbycategoryid() {
+    }
+}
